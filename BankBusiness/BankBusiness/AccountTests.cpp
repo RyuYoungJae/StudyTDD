0b7698862f02856206e59d04671821cf50b433af
YoungJaeRyu <70125455+RyuYoungJae@users.noreply.github.com>
2020-09-12 21:20:53 +0900

    상태가 아닌 구현을 Setup에서 제거

--- BankBusiness/BankBusiness/AccountTests.cpp
+++ BankBusiness/BankBusiness/AccountTests.cpp
@@ -3,35 +3,41 @@
 
 TEST_F(AccountTests, GetBalance_CreateAccountHas10000_EQ10000)
 {
-	EXPECT_EQ(m_account->GetBalance(), 10000);
+	auto account = std::make_shared<Account>(10000);
+	EXPECT_EQ(account->GetBalance(), 10000);
 }
 
 TEST_F(AccountTests, Deposit_Create10000NDeposit1000_Total11000)
 {
-	m_account->Deposit(1000);
-	EXPECT_EQ(m_account->GetBalance(), 11000);
+	auto account = std::make_shared<Account>(10000);
+	account->Deposit(1000);
+	EXPECT_EQ(account->GetBalance(), 11000);
 }
 
 TEST_F(AccountTests, Deposit_Create10000NDepositMinus_10000)
 {
-	m_account->Deposit(-1000);
-	EXPECT_EQ(m_account->GetBalance(), 10000);
+	auto account = std::make_shared<Account>(10000);
+	account->Deposit(-1000);
+	EXPECT_EQ(account->GetBalance(), 10000);
 }
 
 TEST_F(AccountTests, Deposit_Create10000NDeposit0_10000)
 {
-	m_account->Deposit(0);
-	EXPECT_EQ(m_account->GetBalance(), 10000);
+	auto account = std::make_shared<Account>(10000);
+	account->Deposit(0);
+	EXPECT_EQ(account->GetBalance(), 10000);
 }
 
 TEST_F(AccountTests, Withdraw_Create10000NWithdraw1000_Total9000)
 {
-	m_account->Withdraw(1000);
-	EXPECT_EQ(m_account->GetBalance(), 9000);
+	auto account = std::make_shared<Account>(10000);
+	account->Withdraw(1000);
+	EXPECT_EQ(account->GetBalance(), 9000);
 }
 
 TEST_F(AccountTests, Withdraw_Create10000NWithdraw11000_10000)
 {
-	m_account->Withdraw(11000);
-	EXPECT_EQ(m_account->GetBalance(), 10000);
+	auto account = std::make_shared<Account>(10000);
+	account->Withdraw(11000);
+	EXPECT_EQ(account->GetBalance(), 10000);
 }
--- BankBusiness/BankBusiness/AccountTests.h
+++ BankBusiness/BankBusiness/AccountTests.h
@@ -4,12 +4,12 @@
 
 class AccountTests : public testing::Test
 {
-public:
-	std::shared_ptr<Account> m_account;
+//public:
+//	std::shared_ptr<Account> m_account;
 
 protected:
 	void SetUp() override
 	{
-		m_account = std::make_shared<Account>(10000);
+		//m_account = std::make_shared<Account>(10000);
 	}
 };
